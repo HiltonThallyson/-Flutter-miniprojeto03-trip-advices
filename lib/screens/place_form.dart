f28265a8a3d04888898a6ad4cb7dd758cf42b67f
HiltonThallyson <hilton.thallyson@hotmail.com>
2022-05-26 15:58:11 -0300

    improved validation but weird behaviour

--- lib/screens/place_form.dart
+++ lib/screens/place_form.dart
@@ -15,39 +15,84 @@ class _PlaceFormState extends State<PlaceForm> {
   final _averageCostController = TextEditingController();
   final _imgURLController = TextEditingController();
 
+  late FocusNode titleNode;
+  late FocusNode avgCostNode;
+  late FocusNode rateNode;
+
   List<String> countries = [];
   String title = '';
   String rate = '';
   String avgCost = '';
   String imageURL = '';
+  bool isRateValid = false;
+  bool isAvgCostValid = false;
+  bool isTitleValid = false;
+
+  static const snackBarCountryError = SnackBar(
+    content: Text('Selecione ao menos 1 pais'),
+    duration: const Duration(seconds: 1),
+  );
+
+  @override
+  void initState() {
+    titleNode = FocusNode();
+    avgCostNode = FocusNode();
+    rateNode = FocusNode();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    titleNode.dispose();
+    avgCostNode.dispose();
+    rateNode.dispose();
+    super.dispose();
+  }
 
   void _validateForm() {
+    if (countries.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(snackBarCountryError);
+    }
+    if (!isTitleValid) {
+      FocusScope.of(context).requestFocus(titleNode);
+    } else if (!isRateValid) {
+      FocusScope.of(context).requestFocus(rateNode);
+    } else if (!isAvgCostValid) {
+      FocusScope.of(context).requestFocus(avgCostNode);
+    }
     print(title);
     print(rate);
     print(avgCost);
-  }
-
-  void _validateTitle() {
-    print('why?');
-    if (_titleController.text != '') {
-      setState(() {
-        title = _titleController.text;
-      });
+    if (title != '' && rate != '' && avgCost != '' && countries.isNotEmpty) {
+      print(title);
+      print(rate);
+      print(avgCost);
+    } else {
+      print('Invalido');
     }
   }
 
   void _validateRate() {
-    if (_rateController.text != '') {
-      setState(() {
+    setState(() {
+      if (_rateErrorText == null) {
         rate = _rateController.text;
-      });
-    }
+        isRateValid = true;
+      } else {
+        isRateValid = false;
+      }
+    });
   }
 
   void _validateCost() {
-    if (_averageCostController.text != '') {
+    if (_avgCostErrorText == null) {
       setState(() {
         avgCost = _averageCostController.text;
+        isAvgCostValid = true;
+      });
+    } else {
+      setState(() {
+        isAvgCostValid = false;
       });
     }
   }
@@ -62,6 +107,39 @@ class _PlaceFormState extends State<PlaceForm> {
         countries.add(id);
       });
     }
+  }
+
+  String? get _titleErrorText {
+    if (_titleController.text == '') {
+      return 'Insira um valor';
+    }
+    return null;
+  }
+
+  String? get _rateErrorText {
+    double? rateDouble = double.tryParse(_rateController.text);
+    if (_rateController.text == '') {
+      return 'Insira um valor';
+    }
+    if (rateDouble != null) {
+      if (rateDouble < 0 || rateDouble > 5) {
+        return 'Valor invalido';
+      }
+    }
+    return null;
+  }
+
+  String? get _avgCostErrorText {
+    double? avgCostDouble = double.tryParse(_averageCostController.text);
+    if (_averageCostController.text == '') {
+      return 'Insira um valor';
+    }
+    if (avgCostDouble != null) {
+      if (avgCostDouble < 0) {
+        return 'Valor invalido';
+      }
+    }
+    return null;
   }
 
   DropdownMenuItem<String> buildMenuItem(String item) {
@@ -85,24 +163,40 @@ class _PlaceFormState extends State<PlaceForm> {
               child: Column(
             children: [
               TextField(
-                controller: _titleController,
-                decoration: InputDecoration(labelText: 'Titulo'),
-                onChanged: (_) => _validateTitle,
-                onSubmitted: (value) => _validateTitle,
-              ),
+                  controller: _titleController,
+                  autofocus: true,
+                  focusNode: titleNode,
+                  decoration: InputDecoration(
+                      labelText: 'Titulo', errorText: _titleErrorText),
+                  onChanged: (_) {
+                    setState(() {
+                      if (_titleErrorText == null) {
+                        isTitleValid = true;
+                        title = _titleController.text;
+                      } else {
+                        isTitleValid = false;
+                      }
+                    });
+                  }),
               TextField(
                 controller: _rateController,
-                decoration: InputDecoration(labelText: 'Nota'),
+                focusNode: rateNode,
+                decoration: InputDecoration(
+                    labelText: 'Nota',
+                    hintText: 'Insira uma nota entre 0 e 5. Ex.: 3.5',
+                    errorText: _rateErrorText),
                 keyboardType: TextInputType.number,
                 onChanged: (_) => _validateRate(),
-                onSubmitted: (_) => _validateRate(),
               ),
               TextField(
                 controller: _averageCostController,
-                decoration: InputDecoration(labelText: 'Custo medio'),
+                focusNode: avgCostNode,
+                decoration: InputDecoration(
+                    labelText: 'Custo medio',
+                    hintText: 'Insira um valor em reais',
+                    errorText: _avgCostErrorText),
                 keyboardType: TextInputType.number,
                 onChanged: (_) => _validateCost(),
-                onSubmitted: (_) => _validateCost(),
               ),
               SizedBox(
                 height: 20,
