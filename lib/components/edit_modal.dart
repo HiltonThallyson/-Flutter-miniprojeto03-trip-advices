622016eb92aefc5ccacabdc908ac2469e7b41c2c
HiltonThallyson <hilton.thallyson@hotmail.com>
2022-05-28 23:12:27 -0300

    Finished

--- lib/components/edit_modal.dart
+++ lib/components/edit_modal.dart
@@ -1,6 +1,7 @@
 import 'package:f3_lugares/models/place.dart';
 import 'package:flutter/material.dart';
 import '../data/my_data.dart';
+import 'recommendation_modal.dart';
 
 class EditModal extends StatefulWidget {
   final Place place;
@@ -20,14 +21,14 @@ class _EditModalState extends State<EditModal> {
   List<String> countries = [];
   late String id;
   String title = '';
-  late double rate;
-  late double avgCost;
+  String rate = '';
+  String avgCost = '';
   String imageUrl = '';
-  bool isRateValid = false;
-  bool isAvgCostValid = false;
-  bool isTitleValid = false;
-  bool isPlaceAdded = false;
-  bool isUrlEmpty = true;
+  bool isRateValid = true;
+  bool isAvgCostValid = true;
+  bool isTitleValid = true;
+  bool isPlaceAdded = true;
+  bool isUrlEmpty = false;
   List<String> recommendations = [];
 
   late FocusNode titleNode;
@@ -38,12 +39,12 @@ class _EditModalState extends State<EditModal> {
   @override
   void initState() {
     title = widget.place.titulo;
-    rate = widget.place.avaliacao;
+    rate = widget.place.avaliacao.toString();
     id = widget.place.id;
     countries = widget.place.paises;
     recommendations = widget.place.recomendacoes;
     imageUrl = widget.place.imagemUrl;
-    avgCost = widget.place.custoMedio;
+    avgCost = widget.place.custoMedio.toString();
     _titleController.text = title;
     _rateController.text = rate.toString();
     _imgUrlController.text = imageUrl;
@@ -52,31 +53,129 @@ class _EditModalState extends State<EditModal> {
     avgCostNode = FocusNode();
     rateNode = FocusNode();
     imgUrlNode = FocusNode();
+    oldPlace = widget.place;
     super.initState();
   }
 
   void _submitUpdate() {
-    oldPlace = widget.place;
-    final newPlace = Place(
-        id: oldPlace.id,
-        paises: countries,
-        titulo: title,
-        imagemUrl: imageUrl,
-        recomendacoes: recommendations,
-        avaliacao: rate,
-        custoMedio: rate);
-    if (newPlace.titulo != oldPlace.titulo ||
-        newPlace.avaliacao != oldPlace.avaliacao ||
-        newPlace.custoMedio != oldPlace.custoMedio ||
-        newPlace.paises != oldPlace.paises ||
-        newPlace.imagemUrl != oldPlace.imagemUrl ||
-        newPlace.recomendacoes != oldPlace.recomendacoes) {
-      setState(() {
-        DUMMY_PLACES.removeWhere((place) => place.id == id);
-        DUMMY_PLACES.add(newPlace);
-      });
+    if (countries.isEmpty) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              child: Container(
+                height: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('Selecione ao menos 1 pais'),
+                    ElevatedButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: Text('Ok')),
+                  ],
+                ),
+              ),
+              elevation: 4,
+            );
+          });
+      return;
     }
-    Navigator.pop(context);
+    if (recommendations.isEmpty) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              child: Container(
+                height: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('Cadastre pelo menos 1 recomendacao'),
+                    ElevatedButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: Text('Ok')),
+                  ],
+                ),
+              ),
+              elevation: 4,
+            );
+          });
+      return;
+    }
+    if (!isTitleValid) {
+      FocusScope.of(context).requestFocus(titleNode);
+      return;
+    } else if (!isRateValid) {
+      FocusScope.of(context).requestFocus(rateNode);
+      return;
+    } else if (!isAvgCostValid) {
+      FocusScope.of(context).requestFocus(avgCostNode);
+      return;
+    } else if (isUrlEmpty) {
+      FocusScope.of(context).requestFocus(imgUrlNode);
+      return;
+    } else {
+      final newPlace = Place(
+          id: oldPlace.id,
+          paises: countries,
+          titulo: title,
+          imagemUrl: imageUrl,
+          recomendacoes: recommendations,
+          avaliacao: double.parse(rate),
+          custoMedio: double.parse(avgCost));
+      if (newPlace.titulo != oldPlace.titulo ||
+          newPlace.avaliacao != oldPlace.avaliacao ||
+          newPlace.custoMedio != oldPlace.custoMedio ||
+          newPlace.paises != oldPlace.paises ||
+          newPlace.imagemUrl != oldPlace.imagemUrl ||
+          newPlace.recomendacoes != oldPlace.recomendacoes) {
+        setState(() {
+          DUMMY_PLACES.removeWhere((place) => place.id == id);
+          DUMMY_PLACES.add(newPlace);
+        });
+      }
+      Navigator.pop(context);
+    }
+  }
+
+  String? get _titleErrorText {
+    if (_titleController.text == '') {
+      return 'Insira um valor';
+    }
+    return null;
+  }
+
+  String? get _rateErrorText {
+    double? rateDouble = double.tryParse(_rateController.text);
+    if (_rateController.text.isEmpty) {
+      return 'Insira um valor';
+    }
+    if (rateDouble != null) {
+      if (rateDouble < 0 || rateDouble > 5) {
+        return 'Valor invalido';
+      }
+    }
+    return null;
+  }
+
+  String? get _avgCostErrorText {
+    double? avgCostDouble = double.tryParse(_avgCostController.text);
+    if (_avgCostController.text.isEmpty) {
+      return 'Insira um valor';
+    }
+    if (avgCostDouble != null) {
+      if (avgCostDouble < 0) {
+        return 'Valor invalido';
+      }
+    }
+    return null;
+  }
+
+  String? get _imgErrorText {
+    if (_imgUrlController.text.isEmpty) {
+      return 'Insira uma url da imagem';
+    }
+    return null;
   }
 
   void _countriesHandler(String id) {
@@ -89,71 +188,138 @@ class _EditModalState extends State<EditModal> {
     });
   }
 
+  void _validateRate() {
+    setState(() {
+      rate = _rateController.text;
+      if (_rateErrorText == null) {
+        isRateValid = true;
+      } else {
+        isRateValid = false;
+      }
+    });
+  }
+
+  void _validateCost() {
+    setState(() {
+      avgCost = _avgCostController.text;
+      if (_avgCostErrorText == null) {
+        isAvgCostValid = true;
+      } else {
+        isAvgCostValid = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.only(
-            bottom: MediaQuery.of(context).viewInsets.bottom + 20),
+            bottom: MediaQuery.of(context).viewInsets.bottom + 20, left: 10),
         child: SingleChildScrollView(
           child: Form(
             child: Column(children: [
               TextField(
                 controller: _titleController,
+                focusNode: titleNode,
                 onChanged: (_) => setState(() {
                   title = _titleController.text;
+                  if (_titleErrorText == null) {
+                    isTitleValid = true;
+                  } else {
+                    isTitleValid = false;
+                  }
                 }),
                 decoration: InputDecoration(
                   labelText: 'Titulo',
+                  errorText: _titleErrorText,
                 ),
               ),
               TextField(
                 controller: _rateController,
-                onChanged: (_) => setState(() {
-                  rate = double.parse(_rateController.text);
-                }),
+                focusNode: rateNode,
+                onChanged: (_) => _validateRate(),
                 decoration: InputDecoration(
                   labelText: 'Nota',
+                  errorText: _rateErrorText,
+                  hintText: 'Insira uma nota entre 0 e 5. Ex.: 3.5',
                 ),
               ),
               TextField(
                 controller: _avgCostController,
-                onChanged: (_) => setState(() {
-                  avgCost = double.parse(_avgCostController.text);
-                }),
+                focusNode: avgCostNode,
+                onChanged: (_) => _validateCost(),
                 decoration: InputDecoration(
                   labelText: 'Custo',
+                  errorText: _avgCostErrorText,
+                  hintText: 'Insira um valor em reais',
                 ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Selecione o pais'),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    child: Wrap(
-                        spacing: 5,
-                        runSpacing: 5,
-                        children: DUMMY_COUNTRIES
-                            .map(
-                              (country) => FilterChip(
-                                label: Text(country.title),
-                                onSelected: (_) =>
-                                    _countriesHandler(country.id),
-                                backgroundColor:
-                                    Theme.of(context).colorScheme.tertiary,
-                                selectedColor:
-                                    Theme.of(context).colorScheme.primary,
-                                selected: countries.contains(country.id),
-                              ),
-                            )
-                            .toList()),
-                  ),
-                  ElevatedButton(
-                      onPressed: () => _submitUpdate(),
-                      child: Text('Atualizar'))
-                ],
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Selecione o pais'),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      child: Wrap(
+                          spacing: 5,
+                          runSpacing: 5,
+                          children: DUMMY_COUNTRIES
+                              .map(
+                                (country) => FilterChip(
+                                  label: Text(country.title),
+                                  onSelected: (_) =>
+                                      _countriesHandler(country.id),
+                                  backgroundColor:
+                                      Theme.of(context).colorScheme.tertiary,
+                                  selectedColor:
+                                      Theme.of(context).colorScheme.primary,
+                                  selected: countries.contains(country.id),
+                                ),
+                              )
+                              .toList()),
+                    ),
+                    TextField(
+                        enableInteractiveSelection: true,
+                        controller: _imgUrlController,
+                        focusNode: imgUrlNode,
+                        decoration: InputDecoration(
+                            labelText: 'Link da imagem',
+                            errorText: _imgErrorText),
+                        onChanged: (_) => setState(() {
+                              if (_imgErrorText == null) {
+                                isUrlEmpty = false;
+                                imageUrl = _imgUrlController.text;
+                              } else {
+                                isUrlEmpty = true;
+                              }
+                            })),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(child: Text('Recomendacoes')),
+                        TextButton(
+                            onPressed: () {
+                              showModalBottomSheet(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return RecommendationModal(recommendations);
+                                  });
+                            },
+                            child: Text('Gerenciar recomendacao'))
+                      ],
+                    ),
+                    ElevatedButton(
+                        onPressed: () => _submitUpdate(),
+                        child: Text('Atualizar'))
+                  ],
+                ),
               ),
             ]),
           ),
